6ead4e21874962fe5005ab62e14835a3f5601526
Michael Davis <mhdavis1993@gmail.com>
2017-08-16 14:08:25 -0400

    friends.js: Added friendsArray and populated with people

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+let friendsArray = [
+  {
+    name: "Bob Johnson",
+    photo: "https://d30y9cdsu7xlg0.cloudfront.net/png/15724-200.png",
+    scores: ["5", "1", "4"]
+  },
+  {
+    name: "Betty Withers",
+    photo: "https://d30y9cdsu7xlg0.cloudfront.net/png/15724-200.png",
+    scores: ["4", "3", "5"]
+  },
+  {
+    name: "Jane Doe",
+    photo: "https://d30y9cdsu7xlg0.cloudfront.net/png/15724-200.png",
+    scores: ["5", "2", "4"]
+  }
+];
+
+module.exports = friendsArray;
