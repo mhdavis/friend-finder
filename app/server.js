1d70fdc3e4c9693e0218075ba0d42fb55575e51b
Michael Davis <mhdavis1993@gmail.com>
2017-08-16 16:57:46 -0400

    server.js: wrote out server.js

---
+++ app/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+let app = express();
+
+let PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
