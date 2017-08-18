bc6b4d0bbfebfdcfdcc6cf82ca7612cf11c0b2af
Michael Davis <mhdavis1993@gmail.com>
2017-08-17 23:38:21 -0400

    Altered all import statements to require

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,4 +1,4 @@
-import friendsData from '../data/friends.js';
+let friendsData = require('../data/friends.js');
 
 module.exports = function(app) {
   app.get("/api/friends", function (req, res) {
--- app/server.js
+++ app/server.js
@@ -1,5 +1,5 @@
-import express from "express";
-import bodyParser from "body-parser";
+const express = require("express");
+const bodyParser = require("body-parser");
 
 let app = express();
 
@@ -10,8 +10,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-require("./app/routing/apiRoutes")(app);
-require("./app/routing/htmlRoutes")(app);
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
 
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
